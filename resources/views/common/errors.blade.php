845ade6b48c3be5fd55abff958eb168a3d794daa
ngoduyson <ngo.duy.son@framgia.com>
2016-07-06 17:12:32 +0700

    Task List

---
+++ resources/views/common/errors.blade.php
@@ -0,0 +1,15 @@
+<!-- resources/views/common/errors.blade.php -->
+
+@if (count($errors) > 0)
+	<!-- Form error list -->
+	<div class="alert alert-danger">
+		<strong>Whoops! Something went wrong!</strong>
+		<br><br>
+
+		<ul>
+			@foreach ($errors->all() as $error)
+				<li>{{ $error }}</li>
+			@endforeach
+		</ul>
+	</div>
+@endif
